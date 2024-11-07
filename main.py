395e2770fd90aad99c051055643b5511fcc9bb5f
pacheco0 <ferpacvel@gmail.com>
2024-11-06 23:08:31 -0400

    Se hiz re-render de la pantalla

--- main.py
+++ main.py
@@ -18,15 +18,18 @@ def main():
 	player = Player(x, y)
 
 	while True:
+		#Paso 1: Limpia la pantalla color negro
 		screen.fill((0, 0, 0))
 
-		# Draw the player
+		# Paso 2: Dibuja al jugador
+		# Llama player.draw(screen), que usa internamente pygame.draw.polygon
 		player.draw(screen)
 
+		# Paso 3: Actualiza el display a la pantalla
 		pygame.display.flip()
-		dt = fps.tick(60) / 1000
 
-		# Handle quitting the game
+		# Mantiene los FPS y te ayuda a salir del juego
+		dt = fps.tick(60) / 1000
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
