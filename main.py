0d2dc6cbc6e90fa961f033c23e7ebbd922d9160b
pacheco0 <ferpacvel@gmail.com>
2024-11-06 22:33:46 -0400

    Limitando FPS

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ def main():
 	print("Starting asteroids!")
 	print("Screen width: " + str(SCREEN_WIDTH))
 	print("Screen height: " + str(SCREEN_HEIGHT))
+	fps = pygame.time.Clock()
+	dt = 0
 	while True:
+		screen.fill((0, 0, 0))
+		pygame.display.flip()
+		dt = fps.tick(60) / 1000
 		for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					return
-	screen.fill((0, 0, 0))
-	pygame.display.flip()
 if __name__ == "__main__":
 	main()
