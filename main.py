77971ee3d250a9cad57d53678f63f0c6ab549742
pacheco0 <ferpacvel@gmail.com>
2024-11-06 22:25:00 -0400

    Cambio espacios por tabs

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def main():
 	print("Screen height: " + str(SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
-    			if event.type == pygame.QUIT:
-        			return
-		screen.fill((0, 0, 0))
-		pygame.display.flip()
+				if event.type == pygame.QUIT:
+					return
+	screen.fill((0, 0, 0))
+	pygame.display.flip()
 if __name__ == "__main__":
 	main()
