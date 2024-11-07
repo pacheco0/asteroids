13c83858f54d09a1be1e50cdb13e79ef86b3194a
pacheco0 <ferpacvel@gmail.com>
2024-11-06 23:28:58 -0400

    se ajusto def rotate

--- player.py
+++ player.py
@@ -17,7 +17,7 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, pygame.Color("white"), self.triangle(), 2)
     
     def rotate(self, dt):
-        self.dt = PLAYER_TURN_SPEED * dt
+        self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
