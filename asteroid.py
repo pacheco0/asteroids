6972e3e0851f556f90c01335dfe4e6342da127f6
pacheco0 <ferpacvel@gmail.com>
2024-11-09 17:23:15 -0400

    se implemento shot

--- asteroid.py
+++ asteroid.py
@@ -3,8 +3,8 @@ from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        pygame.sprite.Sprite.__init__(self, self.containers)  # Initialize sprite first
-        CircleShape.__init__(self, x, y, radius)  # Then initialize CircleShape
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        CircleShape.__init__(self, x, y, radius)
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
     
--- shot.py
+++ shot.py
@@ -1,5 +1,8 @@
 import pygame
 from circleshape import CircleShape
+from constants import SHOT_RADIUS
 
 class Shot(CircleShape):
-    
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS) # Llama al parent CircleShape con el constructor, X, Y, y SHOT RADIUS
+        self.velocity = pygame.Vector2() # Crea un vector de velocidad
