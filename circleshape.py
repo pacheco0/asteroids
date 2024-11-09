96c77d73eba977ef1c46149bec0c778b8e4b8948
pacheco0 <ferpacvel@gmail.com>
2024-11-09 01:04:53 -0400

    colision

--- circleshape.py
+++ circleshape.py
@@ -17,5 +17,18 @@ class CircleShape(pygame.sprite.Sprite):
         pygame.draw.polygon(screen, pygame.Color("white"), self.triangle(), 2)
 
     def update(self, dt):
-        
         pass
+
+    def collision(self, shape2):
+        # Calcular distancia entre centros
+        distance = self.position.distance_to(shape2.position)
+
+        # Calcular suma de radios
+        radio_sum = self.radius + shape2.radius
+
+        # Comparar con distacia para ver si chocan
+
+        if distance <= radio_sum:
+            return True
+        else:
+            return False 
