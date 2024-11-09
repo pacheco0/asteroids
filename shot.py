ff7a865f395158a83a54b94ac733aaf911b26b87
pacheco0 <ferpacvel@gmail.com>
2024-11-09 17:25:49 -0400

    corrigio shots

--- shot.py
+++ shot.py
@@ -6,3 +6,9 @@ class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS) # Llama al parent CircleShape con el constructor, X, Y, y SHOT RADIUS
         self.velocity = pygame.Vector2() # Crea un vector de velocidad
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, pygame.Color("white"), self.position, self.radius)
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
